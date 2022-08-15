4f46f2a5943f407557e24c2cac756840627c3cff
ltacker <lucas.bertrand.22@gmail.com>
2022-08-15 10:37:31 +0200

    app

---
+++ cmd/testappd/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"os"
+
+	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+	"github.com/ignite/modules/app"
+	"github.com/ignite/modules/cmd"
+)
+
+func main() {
+	rootCmd, _ := cmd.NewRootCmd(
+		app.Name,
+		app.AccountAddressPrefix,
+		app.DefaultNodeHome,
+		app.DefaultChainID,
+		app.ModuleBasics,
+		app.New,
+	)
+	if err := svrcmd.Execute(rootCmd, "", app.DefaultNodeHome); err != nil {
+		os.Exit(1)
+	}
+}
