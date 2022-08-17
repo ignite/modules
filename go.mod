442130f3c35c0fa0ef968e6767b9dfb3da5b72c9
aljo242 <alex@shmeeload.xyz>
2022-08-17 10:26:56 -0400

    remaining fixes

--- go.mod
+++ go.mod
@@ -5,13 +5,24 @@ go 1.18
 require (
 	github.com/cosmos/cosmos-sdk v0.46.0
 	github.com/cosmos/ibc-go/v5 v5.0.0-beta1
+	github.com/gogo/protobuf v1.3.3
+	github.com/golang/protobuf v1.5.2
+	github.com/gorilla/mux v1.8.0
+	github.com/grpc-ecosystem/grpc-gateway v1.16.0
 	github.com/ignite/cli v0.23.0
+	github.com/regen-network/cosmos-proto v0.3.1
 	github.com/spf13/cast v1.5.0
 	github.com/spf13/cobra v1.5.0
 	github.com/spf13/pflag v1.0.5
 	github.com/stretchr/testify v1.8.0
+	github.com/tendermint/fundraising v0.3.1-0.20220613014523-03b4a2d4481a
+	github.com/tendermint/spn v0.2.1-0.20220708132853-26a17f03c072
 	github.com/tendermint/tendermint v0.34.20
 	github.com/tendermint/tm-db v0.6.7
+	google.golang.org/genproto v0.0.0-20220805133916-01dd62135a58
+	google.golang.org/grpc v1.48.0
+	google.golang.org/protobuf v1.28.1
+	gopkg.in/yaml.v2 v2.4.0
 )
 
 require (
@@ -61,24 +72,19 @@ require (
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
 	github.com/godbus/dbus v0.0.0-20190726142602-4481cbc300e2 // indirect
 	github.com/gogo/gateway v1.1.0 // indirect
-	github.com/gogo/protobuf v1.3.3 // indirect
 	github.com/golang/glog v1.0.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/btree v1.0.1 // indirect
 	github.com/google/go-cmp v0.5.8 // indirect
-	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/google/orderedcode v0.0.1 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.0.0-20220520183353-fd19c99a87aa // indirect
 	github.com/googleapis/gax-go/v2 v2.4.0 // indirect
 	github.com/googleapis/go-type-adapters v1.0.0 // indirect
 	github.com/gorilla/handlers v1.5.1 // indirect
-	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0 // indirect
-	github.com/grpc-ecosystem/grpc-gateway v1.16.0 // indirect
 	github.com/gsterjov/go-libsecret v0.0.0-20161001094733-a6f4afe4910c // indirect
 	github.com/gtank/merlin v0.1.1 // indirect
 	github.com/gtank/ristretto255 v0.1.2 // indirect
@@ -118,7 +124,6 @@ require (
 	github.com/prometheus/procfs v0.7.3 // indirect
 	github.com/rakyll/statik v0.1.7 // indirect
 	github.com/rcrowley/go-metrics v0.0.0-20200313005456-10cdbea86bc0 // indirect
-	github.com/regen-network/cosmos-proto v0.3.1 // indirect
 	github.com/rs/cors v1.8.2 // indirect
 	github.com/rs/zerolog v1.27.0 // indirect
 	github.com/sasha-s/go-deadlock v0.2.1-0.20190427202633-1595213edefa // indirect
@@ -143,14 +148,13 @@ require (
 	golang.org/x/xerrors v0.0.0-20220609144429-65e65417b02f // indirect
 	google.golang.org/api v0.84.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/genproto v0.0.0-20220805133916-01dd62135a58 // indirect
-	google.golang.org/grpc v1.48.0 // indirect
-	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/ini.v1 v1.66.6 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	nhooyr.io/websocket v1.8.6 // indirect
 	sigs.k8s.io/yaml v1.3.0 // indirect
 )
 
-replace github.com/gogo/protobuf => github.com/regen-network/protobuf v1.3.3-alpha.regen.1
+replace (
+	github.com/gogo/protobuf => github.com/regen-network/protobuf v1.3.3-alpha.regen.1
+	github.com/tendermint/fundraising => github.com/aljo242/fundraising v0.1.2-0.20220731225846-8ed903159a92
+)
--- testutil/sample/sample.go
+++ testutil/sample/sample.go
@@ -1,13 +1,187 @@
+// Package sample provides methods to initialize sample object of various types for test purposes
 package sample
 
 import (
-	"github.com/cosmos/cosmos-sdk/crypto/keys/ed25519"
+	"math/rand"
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/cosmos/cosmos-sdk/codec"
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	cryptocodec "github.com/cosmos/cosmos-sdk/crypto/codec"
+	cosmosed25519 "github.com/cosmos/cosmos-sdk/crypto/keys/ed25519"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	ibctypes "github.com/cosmos/ibc-go/v5/modules/core/types"
+	"github.com/stretchr/testify/require"
+	fundraising "github.com/tendermint/fundraising/x/fundraising/types"
+	"github.com/tendermint/tendermint/crypto"
+	"github.com/tendermint/tendermint/crypto/ed25519"
+
+	claim "github.com/ignite/modules/x/claim/types"
 )
 
+// Codec returns a codec with preregistered interfaces
+func Codec() codec.Codec {
+	interfaceRegistry := codectypes.NewInterfaceRegistry()
+
+	cryptocodec.RegisterInterfaces(interfaceRegistry)
+	authtypes.RegisterInterfaces(interfaceRegistry)
+	stakingtypes.RegisterInterfaces(interfaceRegistry)
+	banktypes.RegisterInterfaces(interfaceRegistry)
+	ibctypes.RegisterInterfaces(interfaceRegistry)
+	claim.RegisterInterfaces(interfaceRegistry)
+	fundraising.RegisterInterfaces(interfaceRegistry)
+
+	return codec.NewProtoCodec(interfaceRegistry)
+}
+
+// Bool returns randomly true or false
+func Bool(r *rand.Rand) bool {
+	b := r.Intn(100)
+	return b < 50
+}
+
+// Bytes returns a random array of bytes
+func Bytes(r *rand.Rand, n int) []byte {
+	return []byte(String(r, n))
+}
+
+// Uint64 returns a random uint64
+func Uint64(r *rand.Rand) uint64 {
+	return uint64(r.Intn(10000))
+}
+
+// String returns a random string of length n
+func String(r *rand.Rand, n int) string {
+	letter := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+	randomString := make([]rune, n)
+	for i := range randomString {
+		randomString[i] = letter[r.Intn(len(letter))]
+	}
+	return string(randomString)
+}
+
+// AlphaString returns a random string with lowercase alpha char of length n
+func AlphaString(r *rand.Rand, n int) string {
+	letter := []rune("abcdefghijklmnopqrstuvwxyz")
+
+	randomString := make([]rune, n)
+	for i := range randomString {
+		randomString[i] = letter[r.Intn(len(letter))]
+	}
+	return string(randomString)
+}
+
+// PubKey returns a sample account PubKey
+func PubKey(r *rand.Rand) crypto.PubKey {
+	seed := []byte(strconv.Itoa(r.Int()))
+	return ed25519.GenPrivKeyFromSecret(seed).PubKey()
+}
+
+// ConsAddress returns a sample consensus address
+func ConsAddress(r *rand.Rand) sdk.ConsAddress {
+	return sdk.ConsAddress(PubKey(r).Address())
+}
+
 // AccAddress returns a sample account address
-func AccAddress() string {
-	pk := ed25519.GenPrivKey().PubKey()
-	addr := pk.Address()
-	return sdk.AccAddress(addr).String()
+func AccAddress(r *rand.Rand) sdk.AccAddress {
+	addr := PubKey(r).Address()
+	return sdk.AccAddress(addr)
+}
+
+// Address returns a sample string account address
+func Address(r *rand.Rand) string {
+	return AccAddress(r).String()
+}
+
+// ValAddress returns a sample validator operator address
+func ValAddress(r *rand.Rand) sdk.ValAddress {
+	return sdk.ValAddress(PubKey(r).Address())
+}
+
+// OperatorAddress returns a sample string validator operator address
+func OperatorAddress(r *rand.Rand) string {
+	return ValAddress(r).String()
+}
+
+// Validator returns a sample staking validator
+func Validator(t testing.TB, r *rand.Rand) stakingtypes.Validator {
+	seed := []byte(strconv.Itoa(r.Int()))
+	val, err := stakingtypes.NewValidator(
+		ValAddress(r),
+		cosmosed25519.GenPrivKeyFromSecret(seed).PubKey(),
+		stakingtypes.Description{})
+	require.NoError(t, err)
+	return val
+}
+
+// Delegation returns staking delegation with the given address
+func Delegation(t testing.TB, r *rand.Rand, addr string) stakingtypes.Delegation {
+	delAcc, err := sdk.AccAddressFromBech32(addr)
+	require.NoError(t, err)
+
+	return stakingtypes.NewDelegation(
+		delAcc,
+		ValAddress(r),
+		sdk.NewDec(int64(r.Intn(10000))),
+	)
+}
+
+// Coin returns a sample coin structure
+func Coin(r *rand.Rand) sdk.Coin {
+	return sdk.NewCoin(AlphaString(r, 5), sdk.NewInt(r.Int63n(10000)+1))
+}
+
+// CoinWithRange returns a sample coin structure where the amount is a random number between provided min and max values
+// with a random denom
+func CoinWithRange(r *rand.Rand, min, max int64) sdk.Coin {
+	return sdk.NewCoin(AlphaString(r, 5), sdk.NewInt(r.Int63n(max-min)+min))
+}
+
+// CoinWithRangeAmount returns a sample coin structure where the amount is a random number between provided min and max values
+// with a given denom
+func CoinWithRangeAmount(r *rand.Rand, denom string, min, max int64) sdk.Coin {
+	return sdk.NewCoin(denom, sdk.NewInt(r.Int63n(max-min)+min))
+}
+
+// Coins returns a sample coins structure
+func Coins(r *rand.Rand) sdk.Coins {
+	return sdk.NewCoins(Coin(r), Coin(r), Coin(r))
+}
+
+// CoinsWithRange returns a sample coins structure where the amount is a random number between provided min and max values
+func CoinsWithRange(r *rand.Rand, min, max int64) sdk.Coins {
+	return sdk.NewCoins(CoinWithRange(r, min, max), CoinWithRange(r, min, max), CoinWithRange(r, min, max))
+}
+
+// CoinsWithRangeAmount returns a sample coins structure where the amount is a random number between provided min and max values
+// with a set of given denoms
+func CoinsWithRangeAmount(r *rand.Rand, denom1, denom2, denom3 string, min, max int64) sdk.Coins {
+	return sdk.NewCoins(CoinWithRangeAmount(r, denom1, min, max), CoinWithRangeAmount(r, denom2, min, max), CoinWithRangeAmount(r, denom3, min, max))
+}
+
+// TotalSupply returns a sample coins structure where each denom's total supply is within the default
+// allowed supply range
+//func TotalSupply(r *rand.Rand) sdk.Coins {
+//	return CoinsWithRange(r, campaign.DefaultMinTotalSupply.Int64(), campaign.DefaultMaxTotalSupply.Int64())
+//}
+
+// Duration returns a sample time.Duration between a second and 21 days
+func Duration(r *rand.Rand) time.Duration {
+	return time.Duration(r.Int63n(int64(time.Hour*24*21-time.Second))) + time.Second
+}
+
+// DurationFromRange returns a sample time.Duration between the min and max values provided
+func DurationFromRange(r *rand.Rand, min, max time.Duration) time.Duration {
+	return time.Duration(r.Int63n(int64(max-min))) + min
+}
+
+// Int returns a sample sdk.Int
+func Int(r *rand.Rand) sdk.Int {
+	return sdk.NewInt(r.Int63())
 }
