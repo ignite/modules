5b02baa55618b17fc25bfedd406e22387c6d50f3
Alex Johnson <alex.johnson@tendermint.com>
2022-09-06 11:23:03 -0400

    feat: alias sdk error types (#43)
    * add tool files
    
    * update lint and format targets
    
    * create alias
    
    * replace with alias
    
    * format
    
    * add comment

--- x/claim/keeper/msg_claim_initial.go
+++ x/claim/keeper/msg_claim_initial.go
@@ -3,7 +3,8 @@ package keeper
 import (
 	"context"
 
-	sdkerrors "cosmossdk.io/errors"
+	"github.com/ignite/modules/errors"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/ignite/modules/x/claim/types"
@@ -22,7 +23,7 @@ func (k msgServer) ClaimInitial(goCtx context.Context, msg *types.MsgClaimInitia
 	}
 
 	if err := k.CompleteMission(ctx, initialClaim.MissionID, msg.Claimer); err != nil {
-		return nil, sdkerrors.Wrap(types.ErrMissionCompleteFailure, err.Error())
+		return nil, errors.Wrap(types.ErrMissionCompleteFailure, err.Error())
 	}
 
 	return &types.MsgClaimInitialResponse{}, nil
