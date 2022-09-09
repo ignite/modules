3f95361f99095b6bf235eaea21234a5a3fa69312
aljo242 <alex@shmeeload.xyz>
2022-09-09 11:11:13 -0400

    move files

---
+++ pkg/errors/critical_test.go
@@ -0,0 +1,17 @@
+package errors_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/ignite/modules/pkg/errors"
+)
+
+func TestCritical(t *testing.T) {
+	require.ErrorIs(t, errors.ErrCritical, errors.Critical("foo"))
+}
+
+func TestCriticalf(t *testing.T) {
+	require.ErrorIs(t, errors.ErrCritical, errors.Criticalf("foo %s", "bar"))
+}
