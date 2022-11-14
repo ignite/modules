2a483d76f94c26497562090642e6bc94c5c524ab
aljo242 <alex@shmeeload.xyz>
2022-11-14 12:03:28 -0500

    temp test fix

--- x/mint/module_test.go
+++ x/mint/module_test.go
@@ -3,35 +3,24 @@ package mint_test
 import (
 	"testing"
 
-	"cosmossdk.io/simapp"
 	simtestutil "github.com/cosmos/cosmos-sdk/testutil/sims"
+	authkeeper "github.com/cosmos/cosmos-sdk/x/auth/keeper"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	sdkminttestutil "github.com/cosmos/cosmos-sdk/x/mint/testutil"
 	"github.com/stretchr/testify/require"
-	abcitypes "github.com/tendermint/tendermint/abci/types"
-	tmjson "github.com/tendermint/tendermint/libs/json"
-	"github.com/tendermint/tendermint/libs/log"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
-	dbm "github.com/tendermint/tm-db"
 
 	"github.com/ignite/modules/x/mint/types"
 )
 
+// TODO refactor to use mint appConfig
 func TestItCreatesModuleAccountOnInitBlock(t *testing.T) {
-	db := dbm.NewMemDB()
-	app := simapp.NewSimApp(log.NewNopLogger(), db, nil, true, simtestutil.EmptyAppOptions{})
+	var accountKeeper authkeeper.AccountKeeper
 
-	genesisState := simapp.GenesisStateWithSingleValidator(t, app)
-	stateBytes, err := tmjson.Marshal(genesisState)
+	app, err := simtestutil.SetupAtGenesis(sdkminttestutil.AppConfig, &accountKeeper)
 	require.NoError(t, err)
 
-	app.InitChain(
-		abcitypes.RequestInitChain{
-			AppStateBytes: stateBytes,
-			ChainId:       "test-chain-id",
-		},
-	)
-
 	ctx := app.BaseApp.NewContext(false, tmproto.Header{})
-	acc := app.AccountKeeper.GetAccount(ctx, authtypes.NewModuleAddress(types.ModuleName))
+	acc := accountKeeper.GetAccount(ctx, authtypes.NewModuleAddress(types.ModuleName))
 	require.NotNil(t, acc)
 }
