cbb8dbedb6fab7ffb8db806d655baa24542591db
jeronimoalbi <jeronimo.albi@tendermint.com>
2023-04-25 12:28:34 +0200

    fix: change toold dependencies to be able to run app using ignite CLI

--- tools/tools.go
+++ tools/tools.go
@@ -1,17 +1,11 @@
 //go:build tools
-// +build tools
 
 package tools
 
-// Manage tool dependencies via go.mod.
-//
-// https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
-// https://github.com/golang/go/issues/25922
-
-//nolint:all
 import (
-	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
-	_ "golang.org/x/tools/cmd/goimports"
-	_ "golang.org/x/vuln/cmd/govulncheck"
-	_ "mvdan.cc/gofumpt"
+	_ "github.com/cosmos/gogoproto/protoc-gen-gocosmos"
+	_ "github.com/golang/protobuf/protoc-gen-go"
+	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
+	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
+	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2"
 )
