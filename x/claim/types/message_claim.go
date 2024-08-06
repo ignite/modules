79f1d8d3b5307f5381abbb29249785b4ba8802bc
Danilo Pantani <danpantani@gmail.com>
2024-08-06 20:33:30 +0200

    remove unused code

--- x/claim/types/message_claim.go
+++ x/claim/types/message_claim.go
@@ -1,12 +1,6 @@
 package types
 
-import (
-	sdk "github.com/cosmos/cosmos-sdk/types"
-
-	"github.com/ignite/modules/pkg/errors"
-)
-
-var _ sdk.Msg = &MsgClaim{}
+import sdk "github.com/cosmos/cosmos-sdk/types"
 
 func NewMsgClaim(claimer string, missionID uint64) *MsgClaim {
 	return &MsgClaim{
@@ -15,25 +9,6 @@ func NewMsgClaim(claimer string, missionID uint64) *MsgClaim {
 	}
 }
 
-func (msg *MsgClaim) Route() string {
-	return RouterKey
-}
-
 func (msg *MsgClaim) Type() string {
 	return sdk.MsgTypeURL(&MsgClaim{})
-}
-
-func (msg *MsgClaim) GetSigners() []sdk.AccAddress {
-	creator, err := sdk.AccAddressFromBech32(msg.Claimer)
-	if err != nil {
-		panic(err)
-	}
-	return []sdk.AccAddress{creator}
-}
-
-func (msg *MsgClaim) ValidateBasic() error {
-	if _, err := sdk.AccAddressFromBech32(msg.Claimer); err != nil {
-		return errors.Wrapf(errors.ErrInvalidAddress, "invalid creator address (%s)", err)
-	}
-	return nil
 }
