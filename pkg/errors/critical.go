074d16791470e730d10ded2f87e6961bce7f89a7
Danilo Pantani <danpantani@gmail.com>
2024-09-30 22:05:53 -0300

    critical namesace

--- pkg/errors/critical.go
+++ pkg/errors/critical.go
@@ -5,9 +5,9 @@ import (
 	"fmt"
 )
 
-const codespace = "CRITICAL"
+const codespace = "MODULES-CRITICAL"
 
-var ErrCritical = Register(codespace, 2, "the state of the blockchain is inconsistent or an invariant is broken")
+var ErrCritical = Register(codespace, 4, "the state of the blockchain is inconsistent or an invariant is broken")
 
 // Critical handles and/or returns an error in case a critical error has been encountered:
 // - Inconsistent state
