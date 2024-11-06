8a3f18117729080e5d1012840b69c6aec7c25870
Danilo Pantani <danpantani@gmail.com>
2024-11-06 00:46:24 -0300

    chore: remove redudant error pkg (#115)

--- pkg/errors/critical.go
+++ pkg/errors/critical.go
@@ -3,17 +3,19 @@ package errors
 
 import (
 	"fmt"
+
+	sdkerrors "cosmossdk.io/errors"
 )
 
 const codespace = "MODULES-CRITICAL"
 
-var ErrCritical = Register(codespace, 4, "the state of the blockchain is inconsistent or an invariant is broken")
+var ErrCritical = sdkerrors.Register(codespace, 4, "the state of the blockchain is inconsistent or an invariant is broken")
 
 // Critical handles and/or returns an error in case a critical error has been encountered:
 // - Inconsistent state
 // - Broken invariant
 func Critical(description string) error {
-	return Wrap(ErrCritical, description)
+	return sdkerrors.Wrap(ErrCritical, description)
 }
 
 // Criticalf extends a critical error with additional information.
--- x/claim/keeper/msg_server_claim.go
+++ x/claim/keeper/msg_server_claim.go
@@ -4,9 +4,9 @@ import (
 	"context"
 
 	"cosmossdk.io/collections"
+	sdkerrors "cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
-	"github.com/ignite/modules/pkg/errors"
 	"github.com/ignite/modules/x/claim/types"
 )
 
@@ -14,7 +14,7 @@ func (k msgServer) Claim(ctx context.Context, msg *types.MsgClaim) (*types.MsgCl
 	// retrieve claim record of the user
 	claimRecord, err := k.ClaimRecord.Get(ctx, msg.Claimer)
 	if err != nil {
-		return &types.MsgClaimResponse{}, errors.Wrapf(
+		return &types.MsgClaimResponse{}, sdkerrors.Wrapf(
 			types.ErrClaimRecordNotFound,
 			"claim record not found for address %s: %s",
 			msg.Claimer,
@@ -24,7 +24,7 @@ func (k msgServer) Claim(ctx context.Context, msg *types.MsgClaim) (*types.MsgCl
 
 	// check if the claim is an initial claim
 	initialClaim, err := k.InitialClaim.Get(ctx)
-	if err != nil && errors.IsOf(err, collections.ErrNotFound) {
+	if err != nil && sdkerrors.IsOf(err, collections.ErrNotFound) {
 		return nil, err
 	} else if err == nil {
 		if initialClaim.MissionId == msg.MissionId {
@@ -46,7 +46,7 @@ func (k msgServer) Claim(ctx context.Context, msg *types.MsgClaim) (*types.MsgCl
 	airdropStart := params.AirdropStart
 	blockTime := sdk.UnwrapSDKContext(ctx).BlockTime()
 	if blockTime.Before(airdropStart) {
-		return &types.MsgClaimResponse{}, errors.Wrapf(
+		return &types.MsgClaimResponse{}, sdkerrors.Wrapf(
 			types.ErrAirdropStartNotReached,
 			"airdrop start not reached: %s",
 			airdropStart.String(),
